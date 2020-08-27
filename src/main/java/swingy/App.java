58abcb66667f87732cb3beb93c34f063dc45a2c4
LiamKrieling <lmk310500@gmail.com>
2020-08-27 14:30:33 +0200

    added balanced items to artifact and drop generator

--- src/main/java/swingy/App.java
+++ src/main/java/swingy/App.java
@@ -9,10 +9,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         Artifact drop = Artifact.getArtifact();
         System.out.println(drop.getType());
-        System.out.println(drop.getName());
+        System.out.println(drop.getName() + ", rarity :" + drop.getRarity());
         System.out.println("armour :" + drop.getArmour());
         System.out.println("attack :" + drop.getDamage());
         System.out.println("hp :" + drop.getHp());
--- src/main/java/swingy/model/Artifact.java
+++ src/main/java/swingy/model/Artifact.java
@@ -8,6 +8,7 @@ public class Artifact {
     private static int damage;
     private static int hp;
     private static int armour;
+    private static int rarity;
 
     Artifact(){
         String[] drop = Item.item().getDrop();
@@ -15,6 +16,7 @@ public class Artifact {
         damage = Integer.parseInt(drop[1]);
         armour = Integer.parseInt(drop[2]);
         hp = Integer.parseInt(drop[3]);
+        rarity = Integer.parseInt(drop[4]);
         type = drop[5];
     }
 
@@ -36,22 +38,62 @@ public class Artifact {
     public String getType(){
         return type;
     }
+    public int getRarity(){
+        return rarity;
+    }
 }
 
 class Item {
     private static Item item = new Item();
     private static String[][] items = {
             // name , attack , armour , hp , rarity , class
-            {"gunblade","100","0","0","5","weapon"},
-            {"shotgun","100","0","0","5","weapon"},
-            {"meth","32","0","-20","2", "weapon"},
-            {"ludens echo","13","0","0","2","weapon"},
-            {"kevins heart","16","0","13","2","weapon"},
-            {"wooden stick","9","0","0","1","weapon"},
-            {"Armour1","0","10","0","3","armour"},
-            {"Armour2","0","14","0","1","armour"},
-            {"helm1","0","0","16","3","helm"},
-            {"helm2","0","0","4","2","helm"}
+            // weapons
+            {"meth","32","0","-20","3", "weapon"},
+            {"Rusted Spear","8","0","0","1","weapon"},
+            {"Bone Spike","8","0","0","1","weapon"},
+            {"Ludens Echo","13","0","0","2","weapon"},
+            {"Two String Bow","14","0","0","2","weapon"},
+            {"Attack dog","25","0","0","3","weapon"},
+            {"Susanoo","28","0","0","3","weapon"},
+            {"RTX 2080","29","0","0","3","weapon"},
+            {"Chair","30","0","0","3","weapon"},
+            {"Rocket arrows","23","0","0","3","weapon"},
+            {"LTT bottle","26","0","0","3","weapon"},
+            {"Simp","30","0","0","3","weapon"},
+            {"Scissor Blades","43","0","0","4","weapon"},
+            {"Shurikens","40","0","0","4","weapon"},
+            {"Excalibur","40","0","0","4","weapon"},
+            {"CSGO karambit","40","0","0","4","weapon"},
+            {"Hellblade","100","0","0","5","weapon"},
+            {"Carona Gun","158","0","0","5","weapon"},
+            {"Kirito Dual blades","256","0","0","5","weapon"},
+            {"Deamon Dweller","250","-5","0","5","weapon"},
+            // armour
+            {"Scouts Uniform","0","5","0","1","armour"},
+            {"Tattered Rangs","0","5","0","1","armour"},
+            {"Bronze Chestplate","0","7","0","2","armour"},
+            {"Red Pants","0","7","0","2","armour"},
+            {"Chieftians Armour","0","15","0","3","armour"},
+            {"Warriors ShoulderPad","0","15","0","3","armour"},
+            {"Elon Musk Hoodie","0","20","0","4","armour"},
+            {"B.R.A (battle ready armour)","0","30","0","4","armour"},
+            {"Trumps Wall tatoo","0","80","0","5","armour"},
+            {"Anime Waifu Sidekick","10","70","0","5","armour"},
+            {"Incursio","40","100","-200","5","armour"},
+            {"Nakagami","480","300","-600","5","armour"},
+            {"Yummi","10","100","200","5","armour"},
+            // helm
+            {"Mushroom Cap","0","0","10","1","helm"},
+            {"Ashes Cap","0","0","10","1","helm"},
+            {"Methheads Beanie","0","0","20","2","helm"},
+            {"Waifu Panties","0","0","20","2","helm"},
+            {"Kights Helm","0","0","40","3","helm"},
+            {"Anime Hairstyle","0","0","40","3","helm"},
+            {"Nyan cat","0","0","80","4","helm"},
+            {"Senpai Glasses","5","0","70","4","helm"},
+            {"Naruto Headband","0","0","300","5","helm"},
+            {"Rinnegun","40","0","150","5","helm"},
+            {"Amaterasu","300","0","-50","5","helm"},
     };
     public static Item item(){
         return (item);
